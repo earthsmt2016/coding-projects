5229c4f2327a82f965da36e172d084fe7393453e
Sean-Michael Tulloch <earthsmt15@ubuntu.(none)>
2016-10-09 16:28:01 +0100

    Issue_1: Set up Github directories to help manage freelance side projects in various languages

---
+++ php-projects/testing-env/helloWorld.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Simple script to say the infamous Hello World and output it to the terminal
+ */
+
+print "Hello World\n";
+
+?>
---
+++ python-projects/testing-env/helloWorld.py
@@ -0,0 +1,5 @@
+"""
+Simple script to output Hello World to the console.
+"""
+
+print "Hello World!";
